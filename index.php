f8c895741379c1770a3bb6fee1fc06dfd1c6895d
hacker1999 <tz46.78@gmail.com>
2013-11-22 18:57:58 +0400

    .

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 
 header('Content-Type: text/plain; charset=utf-8');
 // определяем папку в которой находится скрипт относительно корня сайта
-define('ROOT_DIR', dirname($_SERVER['SCRIPT_NAME']));
-echo 'ROOT_DIR is ' . ROOT_DIR . "\r\n";
-list($rel_path, ) = explode('?', $_SERVER['REQUEST_URI']);
-// убираем повторы слешей
-$rel_path = preg_replace('#/{2,}#', '/', $rel_path);
-if (ROOT_DIR != '/') {
-    $rel_path = substr($rel_path, strlen(ROOT_DIR));
+$root_dir = dirname($_SERVER['SCRIPT_NAME']);
+if ($root_dir != '/') {
+    $root_dir .= '/';
 }
+define('ROOT_DIR', $root_dir);
+echo 'ROOT_DIR is ' . ROOT_DIR . "\r\n";
+list($path, ) = explode('?', $_SERVER['REQUEST_URI']);
+// убираем повторы слешей
+$path = preg_replace('#/{2,}#', '/', $path);
+$rel_path = substr($path, strlen(ROOT_DIR));
 echo '$rel_path is ' . $rel_path;
